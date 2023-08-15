3c6ddb4b53730f8812d65299f2f0924f58ea08dc
momoi <momoideh.123@yahoo.com>
2023-08-15 13:52:16 +0300

    init

--- components/Home.jsx
+++ components/Home.jsx
@@ -1,53 +1,43 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { View, Text, Button, StyleSheet } from 'react-native';
 import CounterComponent from './CounterComponent';
 
-class Home extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: 0,
-    };
-  }
+const Home = () => {
+  const [total, setTotal] = useState(0);
 
-  handleTotalChange = (value) => {
-    this.setState(prevState => ({
-      total: prevState.total + value,
-    }));
+  const handleTotalChange = (value) => {
+    setTotal(total + value);
   };
 
-  handleShowTotal = () => {
-    alert(`Total Count: ${this.state.total}`);
+  const handleShowTotal = () => {
+    alert(`Total Count: ${total}`);
   };
 
-  render() {
-    const { total } = this.state;
-
-    return (
-      <View style={styles.container}>
-        <View style={styles.box}>
-          <CounterComponent
-            name="1"
-            onChange={this.handleTotalChange}
-          />
-        </View>
-        <View style={styles.box}>
-          <CounterComponent
-            name="2"
-            onChange={this.handleTotalChange}
-          />
-        </View>
-        <View style={styles.box}>
-          <CounterComponent
-            name="3"
-            onChange={this.handleTotalChange}
-          />
-        </View>
-        <Text style={styles.total}>Total Count: {total}</Text>
+  return (
+    <View style={styles.container}>
+      <View style={styles.box}>
+        <CounterComponent
+          name="1"
+          onChange={handleTotalChange}
+        />
       </View>
-    );
-  }
-}
+      <View style={styles.box}>
+        <CounterComponent
+          name="2"
+          onChange={handleTotalChange}
+        />
+      </View>
+      <View style={styles.box}>
+        <CounterComponent
+          name="3"
+          onChange={handleTotalChange}
+        />
+      </View>
+      <Text style={styles.total}>Total Count: {total}</Text>
+     
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -68,3 +58,5 @@ const styles = StyleSheet.create({
 });
 
 export default Home;
+
+
